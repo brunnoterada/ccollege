6090d256b95d66d0709ecf10f914bbc376deb762
brunnoterada <brunoterada99@gmail.com>
2021-05-04 13:33:00 -0300

    exercicio 1 finalizado

--- exercicio1/exercicio1/Origem.cpp
+++ exercicio1/exercicio1/Origem.cpp
@@ -5,9 +5,9 @@
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	char nome[30], *pnome;
-	float salario, *psalario, oliquota, salarioreduzido;
-	
-	printf("Digite seu nome: ");
+	float salario, *psalario, oliquota, *poliquota, salarioreduzido;
+
+	printf("Digite seu Nome Completo: ");
 	gets_s(nome);
 
 	printf("Digite seu salario: ");
@@ -15,14 +15,68 @@ int main() {
 
 	pnome = nome;
 	psalario = &salario;
-
+	
 
 	if (1903.98 >= *psalario)
-	printf("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\nOlá %s!\nSeu salário é de R$%.2f \n*Você está insento da Oliquota*\nO valor a ser reduzido do seu salário é de : R$0\nSalário final: R$%.2f\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-", pnome, *psalario, *psalario);
+	{
+		oliquota = 0;
+		poliquota = &oliquota;
+		printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+		printf("Olá %s!\n", pnome);
+		printf("Salário: %.2f\n",*psalario);
+		printf("Oliquota : %% %.2f (insento)\n", *poliquota);
+		printf("Valor a ser reduzido: %.2f\n", *psalario**poliquota);
+		printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+
+	}
 	else
 		if (2826.65 >= *psalario)
-			printf("%.2f teste", salario*0.075);
-			
+		{
+			oliquota = 0.075;
+			poliquota = &oliquota;
+			printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+			printf("Olá %s!\n", pnome);
+			printf("Salário: %.2f\n", *psalario);
+			printf("Oliquota : %% %.2f\n", *poliquota * 100);
+			printf("Valor a ser reduzido: %.2f\n", *psalario * *poliquota);
+			printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+		}
+		else
+			if (3751.05 >= *psalario)
+			{
+				oliquota = 0.15;
+				poliquota = &oliquota;
+				printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+				printf("Olá %s!\n", pnome);
+				printf("Salário: %.2f\n", *psalario);
+				printf("Oliquota : %% %.2f\n", *poliquota * 100);
+				printf("Valor a ser reduzido: %.2f\n", *psalario * *poliquota);
+				printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+			}
+			else
+				if (4664.68 >= *psalario)
+				{
+					oliquota = 0.225;
+					poliquota = &oliquota;
+					printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+					printf("Olá %s!\n", pnome);
+					printf("Salário: %.2f\n", *psalario);
+					printf("Oliquota : %% %.2f\n", *poliquota * 100);
+					printf("Valor a ser reduzido: %.2f\n", *psalario * *poliquota);
+					printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+				}
+				else 
+				{
+					oliquota = 0.275;
+					poliquota = &oliquota;
+					printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+					printf("Olá %s!\n", pnome);
+					printf("Salário: %.2f\n", *psalario);
+					printf("Oliquota : %% %.2f\n", *poliquota * 100);
+					printf("Valor a ser reduzido: %.2f\n", *psalario * *poliquota);
+					printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+				}
 
-	return 0;
+
+		return 0;
 }
